8453c34cbc1b2c6ce52be2d11bd7597f9600adb3
Felixsie <31508904+Felixsie@users.noreply.github.com>
2018-08-06 03:52:36 -0700

    Finished Prac 4 Chapter 6

---
+++ Programming Excercise 6/CountByThrees.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Lenovo on 8/6/2018.
+ */
+public class CountByThrees
+{
+    public static void main (String args[])
+    {
+        final int START = 3;
+        final int STOP = 300;
+        final int NUMBER_PER_LINE = 30;
+        for(int i = START; i <= STOP; i += START)
+        {
+            System.out.print(i + "  ");
+            if(i % NUMBER_PER_LINE == 0)
+                System.out.println();
+        }
+    }
+}
